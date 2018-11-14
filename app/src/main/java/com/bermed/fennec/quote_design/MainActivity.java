b78d5fe52dfa28ec5ebbeb43291b402d7271c5ce
alfennec <44066991+alfennec@users.noreply.github.com>
2018-11-14 16:25:57 +0000

    v5

--- app/src/main/java/com/bermed/fennec/quote_design/MainActivity.java
+++ app/src/main/java/com/bermed/fennec/quote_design/MainActivity.java
@@ -1,7 +1,6 @@
 package com.bermed.fennec.quote_design;
 
 import android.graphics.Color;
-import android.graphics.PorterDuff;
 import android.graphics.drawable.Drawable;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
@@ -11,11 +10,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
-import java.io.IOException;
-import java.io.InputStream;
 
-import de.hdodenhof.circleimageview.CircleImageView;
-import jp.wasabeef.blurry.Blurry;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,68 +20,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.quote_read);
 
-        ImageView iv = (ImageView) findViewById(R.id.imageView7);
-
-        //CircleImageView cv = (CircleImageView) findViewById(R.id.profile_image);
 
 
 
 
 
-        try {
-            // get input stream
-            InputStream ims = getAssets().open("authors/Bill Gates.jpg");
-            // load image as Drawable
-            Drawable d = Drawable.createFromStream(ims, null);
-            // set image to ImageView
-
-            iv.setImageDrawable(d);
-
-            final int semiTransparentGrey = Color.argb(80, 255, 0, 0);
-
-            iv.setColorFilter(semiTransparentGrey, PorterDuff.Mode.SRC_ATOP);
-        }
-        catch(IOException ex) {
-            return;
-        }
-
-
-        /*Thread thread = new Thread() {
-            @Override
-            public void run() {
-                boolean a = true;
-                try {
-                    while(a) {
-                        sleep(500);
-                        Blurry.with(MainActivity.this)
-                                .radius(25)
-                                .sampling(1)
-                                .color(Color.argb(150, 0, 0, 0))
-                                .async()
-                                .capture(findViewById(R.id.imageView))
-                                .into((ImageView) findViewById(R.id.imageView));
-
-                        a = false;
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        thread.start();
-
-
-        findViewById(R.id.button).setOnClickListener(new View.OnClickListener()
-        {
-            @Override public void onClick(View v)
-            {
-
-                //long startMs = System.currentTimeMillis();
-
-
-            }
-        });*/
 
 
     }
---
+++ app/src/main/java/com/bermed/fennec/quote_design/storage_interne.java
@@ -0,0 +1,72 @@
+package com.bermed.fennec.quote_design;
+
+import android.content.Context;
+import android.util.Log;
+
+import org.json.JSONArray;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class storage_interne {
+
+    private void writeToFile(String data) {
+        try {
+            OutputStreamWriter outputStreamWriter = new OutputStreamWriter(openFileOutput("config.txt", Context.MODE_PRIVATE));
+            outputStreamWriter.write(data);
+            outputStreamWriter.close();
+        }
+        catch (IOException e) {
+            Log.e("Exception", "File write failed: " + e.toString());
+        }
+    }
+
+
+    private String readFromFile(Context ctx) {
+
+        String ret = "";
+
+        try {
+            InputStream inputStream = ctx.openFileInput("config.txt");
+
+            if ( inputStream != null ) {
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                BufferedReader bufferedReader = new BufferedReader(inputStreamReader);
+                String receiveString = "";
+                StringBuilder stringBuilder = new StringBuilder();
+
+                while ( (receiveString = bufferedReader.readLine()) != null ) {
+                    stringBuilder.append(receiveString);
+                }
+
+                inputStream.close();
+                ret = stringBuilder.toString();
+            }
+        }
+        catch (FileNotFoundException e) {
+            Log.e("login activity", "File not found: " + e.toString());
+        } catch (IOException e) {
+            Log.e("login activity", "Can not read file: " + e.toString());
+        }
+
+        return ret;
+    }
+
+    public void read_file()
+    {
+        String str=null;
+        try
+        {
+            JSONArray jarray = new JSONArray(str);
+        }
+        catch (Exception e)
+        {
+
+        }
+
+    }
+}
